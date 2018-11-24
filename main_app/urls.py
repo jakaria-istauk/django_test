bf3f1318182bb3a900ef71bbf1296f3936330933
Mohammad Jakaria Istauk <jakariamd35@gmail.com>
2018-11-24 22:31:00 +0600

    Changed

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.HomeView.as_view(), name="home"),
+    path('', views.HomeView.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
 ]
